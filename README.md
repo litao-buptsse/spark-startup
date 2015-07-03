cc9726cc25d9d2c909b3f262aae6ad3b43ba7c2e
Tao Li <litao@sogou-inc.com>
2015-07-03 15:47:47 +0800

    Update README.md file

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # Spark Startup - Build Your First Spark APP
 
 ---
+
+## SBT打包
+
+```
+$ build/sbt package
+```
+
+Note: 
+
+1. 打包后的JAR位置：target/scala-2.10/spark-startup-scala_2.10-1.0.jar
+2. 建议使用build/sbt工具进行打包，将自动下载scala、sbt、zinc等工具，并自动开启zinc加速编译
+
+## 本地运行
+
+```
+$ spark-submit \
+  	--master local \
+  	--class com.sogou.spark.HelloSparkCore \
+  	spark-startup.jar /user/spark/README.md
+```
+
+## 集群运行
+
+```
+$ spark-submit \
+  	--master yarn-client \
+  	--class com.sogou.spark.HelloSparkCore \
+  	--executor-memory 512M \
+  	--driver-memory 512M \
+  	--num-executors 2 \
+  	--executor-cores 1 \
+  	--queue root.spark_test \
+  	spark-startup.jar /user/spark/README.md
+```
--- src/main/scala/com/sogou/spark/HelloSparkCore.scala
+++ src/main/scala/com/sogou/spark/HelloSparkCore.scala
@@ -6,12 +6,17 @@ import org.apache.spark.{SparkContext, SparkConf}
  * Created by Tao Li on 2015/7/3.
  */
 object HelloSparkCore {
-  def main(args: Array[String]) {
-    val conf = new SparkConf().setAppName("HelloSparkCore").setMaster("local[*]")
+  def main(args: Array[String]): Unit = {
+    if (args.length != 1) {
+      println("usage: spark-submit --master yarn-client " +
+        "--class com.sogou.spark.HelloSparkCore spark-startup.jar <filename>")
+      System.exit(1)
+    }
+
+    val fileName = args(0)
+    val conf = new SparkConf().setAppName("HelloSparkCore")
     val sc = new SparkContext(conf)
-    sc.textFile("README.md")
-      .flatMap(_.split(" ")).map((_, 1))
-      .reduceByKey(_ + _)
+    sc.textFile(fileName).flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _)
       .collect.foreach(println)
   }
 }
