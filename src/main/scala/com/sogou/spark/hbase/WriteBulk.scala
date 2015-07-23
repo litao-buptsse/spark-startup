073f1f3f402c49962b5f1f4ea0acddaede22b7c2
Tao Li <litao@sogou-inc.com>
2015-07-23 19:17:30 +0800

    Add more hbase-rdd demo from hbase-rdd-examples project.

---
+++ src/main/scala/com/sogou/spark/hbase/WriteBulk.scala
@@ -0,0 +1,40 @@
+/* Copyright 2015 UniCredit S.p.A.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.sogou.spark.hbase
+
+import org.apache.spark.{ SparkContext, SparkConf }
+import unicredit.spark.hbase._
+
+object WriteBulk extends App {
+  val name = "Example of write using HFiles"
+
+  lazy val sparkConf = new SparkConf().setAppName(name)
+  lazy val sc = new SparkContext(sparkConf)
+  implicit val config = HBaseConfig() // Assumes hbase-site.xml is on classpath
+  val table = "test-table-bulk"
+  val file = "test-input"
+  val cf = "cf1"
+
+  if (tableExists(table, cf)) {
+    sc.textFile(file)
+      .map({ line =>
+        val Array(k, col1, col2, _) = line split "\t"
+        val content = Map("col1" -> col1, "col2" -> col2)
+
+        k -> content
+      })
+      .toHBaseBulk(table, cf)
+  }
+}
