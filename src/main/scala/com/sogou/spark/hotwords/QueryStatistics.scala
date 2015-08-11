713405ff904098c854a163777ce0f6d688166a82
Tao Li <litao@sogou-inc.com>
2015-08-11 15:43:39 +0800

    Update QueryStatistics.scala

--- src/main/scala/com/sogou/spark/hotwords/QueryStatistics.scala
+++ src/main/scala/com/sogou/spark/hotwords/QueryStatistics.scala
@@ -17,21 +17,25 @@ object QueryStatistics {
 
     val logdate = args(0)
 
-    val rdd: RDD[(String, Map[String, (Array[Byte], Long)])] = sqlContext.sql( s"""
+    implicit val longWriter = new Writes[Long] {
+      def write(data: Long) = Bytes.toBytes(data)
+    }
+
+    val rdd: RDD[(String, Map[String, (Long, Long)])] = sqlContext.sql( s"""
     SELECT pid, query, count(*) AS pv
     FROM custom.scribe_common_wap_nginx
     WHERE logdate='$logdate' AND clean_state='OK' AND pid != '' AND query != ''
     GROUP BY pid, query
     LIMIT 20
       """).map(r =>
-      (r.getAs[String]("pid"), (r.getAs[String]("query"), (Bytes.toBytes(r.getAs[Long]("pv")), logdate.toLong)))
-      ).groupByKey.map(r => (r._1, r._2.toMap))
+      (r.getAs[String]("pid"), (r.getAs[String]("query"), (r.getAs[Long]("pv"), logdate.toLong)))
+      ).groupByKey.mapValues(_.toMap)
 
     implicit val config = HBaseConfig()
     val table = "hotwords_demo"
     val cf = "query"
 
-    // rdd.toHBaseBulk(table, cf)
-    rdd.toHBase(table, cf)
+    rdd.toHBaseBulk(table, cf)
+    // rdd.toHBase(table, cf)
   }
 }
